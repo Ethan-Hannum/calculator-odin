a12f029282960269bc60b2c8c1a0b35a79ee6f97
IknuDeer <ethanhannum10@gmail.com>
2025-10-13 10:27:44 -0700

    Add return statements to math operator functions

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 function add(x, y) {
-
+    return x + y;
 };
 
 function subtract(x, y) {
-
+    return x - y;
 };
 
 function multiply(x, y) {
-
+    return x * y;
 };
 
 function divide(numer, denom) {
-
+    return numer / denom;
 };
