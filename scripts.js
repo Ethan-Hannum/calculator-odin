1a9d7f9943d89875ad4f81f8a50b2cf6d925e429
IknuDeer <ethanhannum10@gmail.com>
2025-10-19 14:20:16 -0700

    Rework calculator logic to work as expected
    Target is now a global let variable so it can be changed and read across
    the code. Anytime a number is set in calculate.numX, an op btn is
    clicked, or an operation is performed calculate.result is added and set
    to true. When a button is clicked the first check is if result in
    calculate exists, if so delete result from calculate and remove the
    highlight from target. If the current target (previous op btn clicked)
    is not equals then set display.textContent to "".
    
    During an operation when num1 and num2 aren't null now the operation
    function such as add will set num1 to operation result and num2 to null.
    Then in the operation function the display will be set to the result.
    Lastly the operator is set to null and if the target.id is equal then
    set num1 to null, as equal was pressed and a new calculation is
    happening.
    
    Also when a num is set the display.textContent is set to itself just to
    make sure it is set to that; and comments were added.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const display = document.querySelector("#display");
 const numBtns = document.querySelectorAll(".num-btn");
 const opBtns = document.querySelectorAll(".op-btn")
+let target;
 
 numBtns.forEach((button) => {
     button.addEventListener("click", getClick);
@@ -16,68 +17,102 @@ const calculate = {
     operator: null,
 }
 
-function add(x, y) {
-    display.textContent = x + y
+// Operation functions will set num1 to given operation and set num2 to null
+
+function add(x, y) { 
+    calculate.num1 = x + y;
+    calculate.num2 = null;
     return x + y;
 };
 
 function subtract(x, y) {
+    calculate.num1 = x - y;
+    calculate.num2 = null;
     return x - y;
 };
 
 function multiply(x, y) {
+    calculate.num1 = x * y;
+    calculate.num2 = null;
     return x * y;
 };
 
 function divide(numer, denom) {
+    calculate.num1 = numer / denom;
+    calculate.num2 = null;
     return numer / denom;
 };
 
+// Finds which operation to run then displays result.
 function operate(op, num1, num2) {
     if (op === "+") {
-        add(num1, num2);
+        display.textContent = add(num1, num2);
     } else if (op === "-") {
-        subtract(num1, num2);
+        display.textContent = subtract(num1, num2);
     } else if (op === "*") {
-        multiply(num1, num2);
+        display.textContent = multiply(num1, num2);
     } else if (op === "/") {
-        divide(num1, num2);
+        display.textContent = divide(num1, num2);
     }
 };
 
+/* If clicked btn is op, then run assignNum. First with num in display to set
+ calculate.numX, and run operate if possible. Then set operator.*/
+// Else add clicked num btn to display.textContent
 function newText(text) {
     const opsigns = "+-*/=";
-    if (opsigns.includes(display.textContent)) {
-        display.textContent = `${text}`
+    if (opsigns.includes(text)) {
+        assignNum(display.textContent);
+        assignNum(text);
     } else {
-        if (opsigns.includes(text)) {
-            assignNum(display.textContent);
-            assignNum(text);
-        } else {
-            display.textContent = `${display.textContent}${text}`;
-        }
+        display.textContent = `${display.textContent}${text}`;
     }
-};
+}
+
 
 function assignNum(displayText) {
     const opsigns = "+-*/=";
+    /* Used when running assignNum(text), sets calculator.operator to the op btn
+    that was clicked if said btn wasn't equals. Sets .result to true*/
     if (opsigns.includes(displayText)) {
         if (calculate.operator === null) {
-            calculate.operator = displayText;
+            if (displayText !== "=") {
+                calculate.operator = displayText;
+            }
+            calculate["result"] = true;
             console.log(`calculate.operator is ${calculate.operator}`);
         }
+    // when op-btn is pressed take the display.textContent and do following:
     } else {
+        /* If num1 is null: Set num1 to display.textContent and make sure that 
+        number is displayed. Set .result to true */
         if (calculate.num1 === null) {
             calculate.num1 = +displayText;
+            display.textContent = displayText;
+            calculate["result"] = true;
             console.log(`num1 is ${calculate.num1}`);
+        /* If num2 is null: Set num2 to display.textContent and make sure that 
+        number is displayed. Set .result to true */
         } else if (calculate.num2 === null) {
             calculate.num2 = +displayText;
+            display.textContent = displayText;
+            calculate["result"] = true;
             console.log(`num2 is ${calculate.num2}`);
         }
     }
 
+    /* Only ran if both num1 and num2 are not null. Should only run
+    if assignNum(display.textContent) is called since num2 will be removed when
+    if statement is ran.*/
     if ((calculate.num1 !== null) && (calculate.num2 !== null)) {
         operate(calculate.operator, calculate.num1, calculate.num2);
+        calculate["result"] = true;
+        calculate.operator = null;
+        /* Lets a new calculation begin, with result from = press will be stored
+        as num1 when new operator is pressed. Unless used clicks new number */
+        if (target.id === "equal") {
+            calculate.num1 = null;
+        }
     }
 }
 
@@ -85,12 +120,24 @@ function getClick(e) {
     const recentClick = e.target.textContent;
     const opsigns = "+-*/=";
 
-    if (opsigns.includes(recentClick)) {
-        const target = document.getElementById(`${e.target.id}`);
-        console.log(target);
-        target.classList.add("highlighted-op");
+    /* If calculate.result exists check if target.id is not equal to equal. If
+    so empty display.*/
+    if ("result" in calculate) {
+        if (target.id !== "equal") {
+            display.textContent = "";
+        }
+        delete calculate.result;
+        target.classList.remove("highlighted-op");
     }
-    // If C is clicked, clear display and reset numbers to null
+
+    // Set target to op-btn that was clicked, if clicked. Add highlight unless =
+    if (opsigns.includes(recentClick)) {
+        target = document.getElementById(`${e.target.id}`);
+        console.log(target);
+        if (target.id !== "equal") target.classList.add("highlighted-op");
+    }
+    // If C is clicked, clear display and reset numbers to null,
+    // If any other button is clicked call newText(recentClick)
     if (recentClick === "C") {
         display.textContent = ``;
         calculate.num1 = null;
